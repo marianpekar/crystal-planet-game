a02efae012d5dd9ceac74b3903b51e573b5590dd
Marian Pekár <marian.pekar@gmail.com>
2020-02-21 18:07:31 +0100

    Add PlayerController and PlayerStates and reorganize folders

---
+++ Assets/Sources/Player/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    KeyCode leftKey;
+
+    [SerializeField]
+    KeyCode rightKey;
+
+    [SerializeField]
+    GameObject body;
+
+    private Vector3 steering;
+
+    void Update()
+    {
+        GatherInputs();
+        Move();
+        TiltBody();
+    }
+
+    void GatherInputs()
+    {
+        if (Input.GetKey(leftKey))
+            steering.x -= PlayerStates.Instance.SteeringSpeed;
+        else if (Input.GetKey(rightKey))
+            steering.x += PlayerStates.Instance.SteeringSpeed;
+        else
+            steering = Vector3.zero;
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * PlayerStates.Instance.FlightSpeed + steering * PlayerStates.Instance.SteeringSpeed * Time.deltaTime);
+    }
+
+    private void TiltBody()
+    {
+        if (steering.x > 0)
+            body.transform.rotation = Quaternion.Slerp(body.transform.rotation,
+                                                        Quaternion.Euler(-PlayerStates.Instance.SteeringTiltAngle),
+                                                        PlayerStates.Instance.SteeringTiltSpeed * Time.deltaTime);
+        else if (steering.x < 0)
+            body.transform.rotation = Quaternion.Slerp(body.transform.rotation,
+                                                        Quaternion.Euler(PlayerStates.Instance.SteeringTiltAngle),
+                                                        PlayerStates.Instance.SteeringTiltSpeed * Time.deltaTime);
+        else
+            body.transform.rotation = Quaternion.Slerp(body.transform.rotation,
+                                                        Quaternion.Euler(Vector3.zero),
+                                                        PlayerStates.Instance.SteeringTiltSpeed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Sources/Player/PlayerStates.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public sealed class PlayerStates
+{
+    // Movement
+    public float FlightSpeed { get; } = 0.1f;
+    public float SteeringSpeed { get; } = 0.1f;
+    public Vector3 SteeringTiltAngle { get; } = new Vector3(0, 12f, 22f);
+    public float SteeringTiltSpeed { get; } = 15f;
+
+
+    private static PlayerStates instance;
+    public static PlayerStates Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new PlayerStates();
+            }
+
+            return instance;
+        }
+    }
+}

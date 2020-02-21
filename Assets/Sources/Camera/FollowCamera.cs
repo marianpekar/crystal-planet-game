ef135d5d032e29c28d9d99d2e148991845c28af0
Marian Pekár <marian.pekar@gmail.com>
2020-02-21 20:31:25 +0100

    Add Ground, Lights and FollowCamera

---
+++ Assets/Sources/Camera/FollowCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField]
+    GameObject target;
+
+    [SerializeField] 
+    float damping = 3f;
+
+    void LateUpdate()
+    {
+        float currentXPosition = transform.position.x;
+        float desiredXPosition = target.transform.position.x;
+        float positionX = Mathf.Lerp(currentXPosition, desiredXPosition, Time.deltaTime * damping);
+
+        transform.position = new Vector3(positionX, transform.position.y, transform.position.z);
+
+        transform.LookAt(target.transform);
+    }
+}
--- Assets/Sources/Player/PlayerStates.cs
+++ Assets/Sources/Player/PlayerStates.cs
@@ -6,7 +6,7 @@ public sealed class PlayerStates
 {
     // Movement
     public float FlightSpeed { get; } = 0.1f;
-    public float SteeringSpeed { get; } = 4f;
+    public float SteeringSpeed { get; } = 6f;
     public Vector3 SteeringTiltAngle { get; } = new Vector3(0, 16f, 22f);
     public float SteeringTiltSpeed { get; } = 8f;
     public float ShakeStrength { get; } = 0.1f;

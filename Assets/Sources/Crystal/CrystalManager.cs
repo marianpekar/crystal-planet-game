52f5bca454a60c6cacb66ab014bd8a0ee6a81f0a
Marian Pekár <marian.pekar@gmail.com>
2020-02-21 21:16:25 +0100

    Add CrystalManager

---
+++ Assets/Sources/Crystal/CrystalManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject crystalPool;
+
+    [SerializeField]
+    Transform crystalSpawnPivot;
+
+    private Transform[] crystals;
+
+    void Start()
+    {
+        crystals = crystalPool.GetComponentsInChildren<Transform>();
+
+        foreach(Transform crystal in crystals)
+        {
+            crystal.position = crystalSpawnPivot.position;
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
